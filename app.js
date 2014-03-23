6610cd30baf57028d3ff913069d61e7fc9bd5c28
Rogelio Guzman <chinog9@gmail.com>
2014-03-23 13:48:06 -0700

    Migrates to hogan

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var routes = require('./routes');
 var users = require('./routes/user');
@@ -12,8 +13,9 @@ var users = require('./routes/user');
 var app = express();
 
 // view engine setup
+app.engine('hogan', cons.hogan);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hogan');
 
 app.use(favicon());
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
-/* GET home page. */
+var cons = require('consolidate');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',
+    partials: {
+      head: 'head',
+      javascripts: 'javascripts',
+      examples: 'examples',
+      'examples/hello_world': 'examples/hello_world'
+    }
+  });
 };
