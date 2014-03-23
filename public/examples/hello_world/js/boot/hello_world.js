188d43d400d5d84dbd50616c7727f2de0a1c9067
Rogelio Guzman <chinog9@gmail.com>
2014-03-23 13:51:07 -0700

    Removes flight template example

--- public/examples/hello_world/js/boot/hello_world.js
+++ public/examples/hello_world/js/boot/hello_world.js
@@ -2,16 +2,10 @@ define(function (require) {
   'use strict';
 
    var HelloWorld = require('examples/hello_world/js/ui/hello_world');
-   var ExampleTemplate = require('component/example_template');
 
   return helloWorldBoot;
 
   function helloWorldBoot() {
-    ExampleTemplate.attachTo(".Example[data-example-name='hello_world']", {
-      codeHtml: '<h1>name</h1>',
-      appHtml: '<h1>result</h1>'
-    });
-
     HelloWorld.attachTo(".Example[data-example-name='hello_world']");
   }
 });
